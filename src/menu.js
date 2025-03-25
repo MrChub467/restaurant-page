fe4a0fc17339bb6dadffb43749d06a2e5b7a295b
MrChub467 <chubster467@gmail.com>
2025-03-25 09:46:19 -0500

    add more content

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,14 @@
 export default (function() {
+    const foodOptions = ["Tire Soup", "Retread Bread", "Oil Beer", "Exhaust Pizza", "Blinker Fluid Pizza", "Filter Pizza"];
     const content = document.querySelector("#content");
     content.innerHTML = "";
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let text = document.createElement("p");
-    text.textContent = "Tire Soup!";
-    card.appendChild(text);
-    content.appendChild(card);
-  });
+    foodOptions.forEach((food) => {
+
+      let card = document.createElement("div");
+      card.classList.add("card");
+      let text = document.createElement("p");
+      text.textContent = food;
+      card.appendChild(text);
+      content.appendChild(card);
+    })
+});
